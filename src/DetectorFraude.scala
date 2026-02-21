d1a0bb37533d75bb5b8a259708f04337170bd8a0
Willicami22 <willicamiher22@gmail.com>
2026-02-20 21:22:29 -0500

    Add fraud detection methods for atypical amounts, impossible locations, and transaction bursts

--- src/DetectorFraude.scala
+++ src/DetectorFraude.scala
@@ -1,12 +1,45 @@
 import domain.{PerfilCliente, Transaccion}
+import traits.{EventoFraude, MontoAtipico, UbicacionImposible, RafagaTransacciones}
 
 object DetectorFraude {
   val mapaPerfiles: Map[String, PerfilCliente] = Dataset.perfiles.map(x => (x.clienteId, x)).toMap
 
   def validarTransaccion(t: Transaccion): Either[String, Transaccion] = {
-    if (t.monto > 0 || !t.clienteId.isEmpty) Right(t)
+    if (t.monto > 0 || t.clienteId.nonEmpty) Right(t)
     else if (t.monto <= 0) Left("Monto invalido")
     else Left("Cliente invalido")
+  }
+
+  def detectarMontoAtipico(t: Transaccion, perfil: PerfilCliente) : Option[EventoFraude] = {
+    if (t.monto > perfil.promedioHistorico * 5 ) Some(MontoAtipico(t.monto, perfil.promedioHistorico))
+    else None
+  }
+
+  def detectarUbicacionImposible(t:Transaccion, historico: List[Transaccion]): Option[EventoFraude] = {
+    val clienteTrans = historico.filter(_.clienteId == t.clienteId).sortBy(_.timeStamp)
+        if (clienteTrans.isEmpty) return None
+
+    val todasTrans = (clienteTrans :+ t).sortBy(_.timeStamp)
+    val ventanas = todasTrans.sliding(2).toList
+
+    ventanas.collectFirst {
+      case List(anterior, actual) if anterior.coordenadas.pais != actual.coordenadas.pais && (actual.timeStamp - anterior.timeStamp) < 60 =>
+        UbicacionImposible(anterior.coordenadas.pais, actual.coordenadas.pais, actual.timeStamp - anterior.timeStamp)
+    }
+  }
+
+  def detectarRafaga(t:Transaccion, historico: List[Transaccion]): Option[EventoFraude] = {
+    val clienteTrans = historico.filter(_.clienteId == t.clienteId).sortBy(_.timeStamp)
+      if (clienteTrans.isEmpty) return None
+
+    val todasTrans = (clienteTrans :+ t).sortBy(_.timeStamp)
+    val ventanas = todasTrans.sliding(5).toList
+
+    
+        ventanas.collectFirst {
+          case ventana if ventana.size == 5 && (ventana.last.timeStamp - ventana.head.timeStamp) <= 5 =>
+            RafagaTransacciones(5, ventana.last.timeStamp - ventana.head.timeStamp)
+        }
   }
 
 }
--- src/traits/EventoFraude.scala
+++ src/traits/EventoFraude.scala
@@ -3,5 +3,5 @@ package traits
 sealed trait EventoFraude
 case class VelocidadExcesiva(cantidadOps: Int, ventanaMinutos: Int) extends EventoFraude
 case class MontoAtipico(monto: Double, promedioHistorico: Double) extends EventoFraude
-case class UbicacionImposible(pais1: String, pais2: String, minutos: Int) extends EventoFraude
-case class RafagaTransacciones(cantidadOps: Int, ventanaMinutos: Int) extends EventoFraude
+case class UbicacionImposible(pais1: String, pais2: String, minutos: Long) extends EventoFraude
+case class RafagaTransacciones(cantidadOps: Int, ventanaMinutos: Long) extends EventoFraude
