abfdaec0f992f2c180e4d079bee15bc8ee3de11b
Willicami22 <willicamiher22@gmail.com>
2026-02-20 20:40:29 -0500

    Add transaction validation logic and customer profile mapping to fraud detector

--- src/DetectorFraude.scala
+++ src/DetectorFraude.scala
@@ -1,3 +1,12 @@
+import domain.{PerfilCliente, Transaccion}
+
 object DetectorFraude {
+  val mapaPerfiles: Map[String, PerfilCliente] = Dataset.perfiles.map(x => (x.clienteId, x)).toMap
+
+  def validarTransaccion(t: Transaccion): Either[String, Transaccion] = {
+    if (t.monto > 0 || !t.clienteId.isEmpty) Right(t)
+    else if (t.monto <= 0) Left("Monto invalido")
+    else Left("Cliente invalido")
+  }
 
 }
